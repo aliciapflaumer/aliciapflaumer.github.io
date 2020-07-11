f1145f3b5e21490184e5b7d860b2e3e01771328b
aliciapflaumer <alpfla7@gmail.com>
2020-07-11 10:59:18 -0400

    Created new Drawing section
    – Added new tab with modals
    - Added three past sketches with captions

--- js/my-javascript.js
+++ js/my-javascript.js
@@ -2,15 +2,24 @@
   "use strict";
 
   $('.photography').hide();
+  $('.drawings').hide();
 
   $('#web-development').click(function() {
     $('.photography').hide();
+    $('.drawings').hide();
     $('.web-development').show()
   });
 
   $('#photography').click(function() {
     $('.web-development').hide();
+    $('.drawings').hide();
     $('.photography').show()
+  });
+
+  $('#drawings').click(function() {
+    $('.drawings').show();
+    $('.web-development').hide();
+    $('.photography').hide()
   });
 
 })(jQuery);
